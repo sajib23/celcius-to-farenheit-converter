ca0608c9ee08ece1b41b1dfabff07b4ba0799719
sajib23 <119812106+sajib23@users.noreply.github.com>
2023-02-24 23:34:17 +0600

    Add files via upload

---
+++ Celius_to_farenheit_converter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+float temp=25;
+printf("hello!Welcome to celcius to farenheit converter\n");
+printf("'Enter the temparature value (in celcius):\n");
+scanf("%f",& temp);
+float ctemp=(1.8 * temp +32);
+printf("the temperature in farenheit is %f\n",ctemp);
+
+
+
+
+
+
+return 0;
+}
